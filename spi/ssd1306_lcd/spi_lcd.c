f87d7171862c61297d3140be391e7fe373b2b317
decryptec <dhsdon47@outlook.com>
2025-05-04 22:56:09 -0700

    probe

--- spi/ssd1306_lcd/spi_lcd.c
+++ spi/ssd1306_lcd/spi_lcd.c
@@ -1,24 +1,29 @@
 #include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/fs.h>
-#include <linux/cdev.h>
 #include <linux/spi/spi.h>
+#include <linux/gpio/consumer.h> // Include for GPIO
+#include <linux/of_gpio.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
 #include <linux/device.h>
+#include <linux/kdev_t.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
 #include <linux/err.h>
 #include <linux/delay.h>
 
-#define DEVICE_NAME "my_lcd"
-#define MAJOR_NUM 300
+#define DEVICE_NAME "ssd1306_spi"
 static struct spi_device *my_spi_device;
 
-// Register information about slave device;
-struct spi_board_info my_spi_device_info = 
-{
-	.modalias = "spi-ssd1306-driver",
-	.max_speed_hz = 10000000, // Minimum clock cycle 100ns, Max = 1/(100*(10^-9))
-	.bus_num = 1,
-	.chip_select = 0,
-	.mode = SPI_MODE_0
+struct my_lcd {
+	struct spi_device *spi;
+	struct gpio_desc *reset_gpio;
+	struct gpio_desc *dc_gpio;
+	u8 *framebuffer;
+	int width;
+	int height;
+	struct cdev cdev;
+	struct device *device;
 };
 
 // Device tree
@@ -29,6 +34,80 @@ static const struct spi_device_id my_ssd1306_id[] = {
 MODULE_DEVICE_TABLE(spi, my_ssd1306_id);
 
 static int my_ssd1306_probe(struct spi_device *spi){
+	struct my_lcd *lcd;
+	int ret;
+
+	dev_info(&spi->dev, "SSD1306 lcd probe start\n");
+
+	/* Allocate driver data */
+	lcd = devm_kzalloc(&spi->dev, sizeof(struct my_lcd), GFP_KERNEL);
+	if (!lcd){
+		dev_err(&spi->dev, "Failed to allocate driver data\n");
+		return -ENOMEM;
+	}
+
+	/* Store spi_device */
+	lcd->spi = spi;
+
+	/* Setup GPIO referencing device tree */
+	lcd->reset_gpio = devm_gpiod_get(&spi->dev, "reset", GPIOD_OUT_HIGH);
+	if (IS_ERR(lcd->reset_gpio)){
+		dev_err(&spi->dev, "Failed to request reset GPIO\n");
+		return -1;
+	}
+
+	lcd->dc_gpio = devm_gpiod_get(&spi->dev, "dc", GPIOD_OUT_HIGH);
+	if (IS_ERR(lcd->dc_gpio)){
+		dev_err(&spi->dev, "Failed to request dc GPIO\n");
+		return -1;
+	}
+
+	/* Width and Height from device tree */
+	ret = of_property_read_u32(spi->dev.of_node, "width", &lcd->width);
+	if (ret) {
+		dev_err(&spi->dev, "Failed to read 'width' from device tree\n");
+		return ret;
+	}
+
+	ret = of_property_read_u32(spi->dev.of_node, "height", &lcd->height);
+	if (ret) {
+		dev_err(&spi->dev, "Failed to read 'height' from device tree\n");
+		return ret;
+	}
+
+	dev_info(&spi->dev, "Display Width: %d, Height: %d\n", lcd->width, lcd->height);
+
+	/* Allocate Framebuffer */
+	lcd->framebuffer = dezm_kazalloc(&spi->dev, (lcd->width * lcd->height / 8), GFP_KERNEL); // 1 bit per pixel
+	if (!lcd->framebuffer){
+		dev_err(&spi->dev, "Failed to allocate framebuffer\n");
+		return -ENOMEM;
+	}
+
+	/* SPI Settings */
+	spi->mode = SPI_MODE_0;
+	spi->bits_per_word = 8;
+
+	ret = of_property_read_u32(spi->dev.of_node, "spi-max-frequency", &spi->max_speed_hz);
+	if (ret){
+		dev_warn(&spi->dev, "failed to read spi-max-frequncy from device tree, using default\n");
+		spi->max_speed_hz = 10000000; // Default 10MHz
+	}
+	dev_info(&spi->dev, "SPI MAX freq: %d Hz\n", spi->max_speed_hz);
+
+	ret = spi_setup(spi);
+	if (ret < 0) {
+		dev_err(&spi->dev, "SPI setup failed: %d\n", ret);
+		return ret;
+	}
+
+	/* Store data in spi_device */
+	spi_set_drvdata(spi, lcd);
+
+	/* LCD Init */
+
+	dev_info(&spi->dev, "SSD1306 LCD probe complete\n");
+
 	return 0;
 }
 
@@ -40,10 +119,11 @@ static struct spi_driver my_ssd1306_driver = {
 	.driver = {
 		.name = "my_ssd1306",
 		.owner = THIS_MODULE,
+		.of_match_table = of_match_ptr(my_ssd1306_id), // DTO
 	},
 	.probe = my_ssd1306_probe,
 	.remove = my_ssd1306_remove,
-	.it_table = my_ssd1306_id,
+	.id_table = my_ssd1306_id,
 };
 
 module_spi_driver(my_ssd1306_driver);
